c698a397d1b7f81b11068570b7de996e9bf5501e
drewandrewd <andrewid10@gmail.com>
2025-03-05 00:11:28 +0500

    Task 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,42 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите 3 числа:");
-        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(15) + 1;
+        }
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
 
-        int a = Integer.parseInt(scanner.nextLine());
-        int b = Integer.parseInt(scanner.nextLine());
-        int c = Integer.parseInt(scanner.nextLine());
-
-        dividerFive(Map.of("a", a, "b", b, "c", c));
-
-        System.out.printf("Результат целочисленного деления a на b: %d\n", a / b);
-        System.out.printf("Результат деления a на b: %f\n", (double) a / b);
-        System.out.printf("Результат деления a на b с округлением в большую сторону: %d\n", (int)  Math.ceil((double) a / b));
-        System.out.printf("Результат деления a на b с округлением в меньшую сторону: %d\n", (int) Math.floor((double) a / b));
-        System.out.printf("Результат деления a на b с математическим округлением: %d\n", Math.round((double) a / b));
-        System.out.printf("Остаток от деления b на c: %d\n", b % c);
-        System.out.printf("Наименьшее значение из a и b: %d\n", Math.min(a, b));
-        System.out.printf("Наибольшее значение из b и c: %d\n", Math.max(b, c));
-    }
-
-    public static void dividerFive(Map<String, Integer> map) {
-        boolean isDividing = false;
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() % 5 == 0) {
-                System.out.printf("%s=%d\n", entry.getKey(), entry.getValue());
-                isDividing = true;
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] == arr[i]) {
+                count++;
+            }
+            else {
+                if (count > 1) {
+                    map.put(arr[i - 1], count);
+                }
+                count = 1;
             }
         }
-        if (!isDividing) {
-            System.out.println("нет значений, кратных 5");
+
+        if (count > 1) {
+            map.put(arr[arr.length - 1], count);
+        }
+
+        if (!map.isEmpty()) {
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                System.out.printf("Число '%d' встречается %d раза\n", entry.getKey(), entry.getValue());
+            }
         }
     }
 }
