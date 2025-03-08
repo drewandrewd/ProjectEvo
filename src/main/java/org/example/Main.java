4e5d5fc3e0e1c52456efc997f829bc5f6b6e84a4
drewandrewd <andrewid10@gmail.com>
2025-03-08 23:15:57 +0500

    Task 8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
+        Map<Integer, List<User>> map = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 1; i <= 5; i++) {
@@ -12,16 +12,23 @@ public class Main {
             String name = scanner.nextLine();
             System.out.println("Введите возраст пользователя " + i);
             int age = Integer.parseInt(scanner.nextLine());
-            users.add(new User(name, age));
+            User newUser = new User(name, age);
+            if (map.containsKey(age)) {
+                List<User> users = map.get(age);
+                users.add(newUser);
+                map.put(age, users);
+            } else {
+                map.put(age, new ArrayList<>(List.of(newUser)));
+            }
         }
 
-        Collections.sort(users, new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return o1.getAge() - o2.getAge();
-            }
-        });
-
-        users.stream().forEach(curr -> System.out.println(curr));
+        System.out.println("Введите требуемый возраст");
+        int age = Integer.parseInt(scanner.nextLine());
+        if (map.containsKey(age)) {
+            List<User> users = map.get(age);
+            users.stream().forEach(curr -> System.out.println(curr));
+        } else {
+            System.out.printf("Пользователь с возрастом '%d' не найден", age);
+        }
     }
 }
