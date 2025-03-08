05f9073949c97653a0bdb4741a54c57cd56ede08
drewandrewd <andrewid10@gmail.com>
2025-03-08 22:50:03 +0500

    Task 7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,27 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя первого пользователя");
-        String nameOfUserOne =  scanner.nextLine();
-        System.out.println("Введите возраст первого пользователя");
-        int ageOfUserOne = Integer.parseInt(scanner.nextLine());
-        User userOne = new User(nameOfUserOne, ageOfUserOne);
 
-        System.out.println("Введите имя второго пользователя");
-        String nameOfUserTwo = scanner.nextLine();
-        System.out.println("Введите возраст второго пользователя");
-        int ageOfUserTwo = Integer.parseInt(scanner.nextLine());
-        User userTwo = new User(nameOfUserTwo, ageOfUserTwo);
-
-        if (userOne.getAge() < userTwo.getAge()) {
-            System.out.println(userOne);
-        } else if (userOne.getAge() > userTwo.getAge()) {
-            System.out.println(userTwo);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Введите имя пользователя " + i);
+            String name = scanner.nextLine();
+            System.out.println("Введите возраст пользователя " + i);
+            int age = Integer.parseInt(scanner.nextLine());
+            users.add(new User(name, age));
         }
+
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        });
+
+        users.stream().forEach(curr -> System.out.println(curr));
     }
 }
