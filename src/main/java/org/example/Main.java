93fa394e48d66e936c34127afa08b202d35723ab
drewandrewd <andrewid10@gmail.com>
2025-03-18 17:25:22 +0500

    Task 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
-import java.util.*;
+import java.util.stream.LongStream;
 
 public class Main {
+
     public static void main(String[] args) {
-        new StudentWithClock(new Student()).learn();
+        System.out.println(getArithmeticProgressionSum(10_000_000, 1_000_000_000));
+    }
+
+    public static long getArithmeticProgressionSum(int a, int b) {
+        return LongStream.range(a, b).sum();
     }
 }
